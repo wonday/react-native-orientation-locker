05eb58ab2087ee51b8254cf4d1684a0c8554a4c5
Wonday <wonday@wonday.org>
2017-08-08 17:21:58 +0800

    fix for RN 0.47.1

--- android/src/main/java/org/wonday/orientation/OrientationPackage.java
+++ android/src/main/java/org/wonday/orientation/OrientationPackage.java
@@ -30,7 +30,7 @@ public class OrientationPackage implements ReactPackage {
         );
     }
 
-    @Override
+    
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
