a6ae45ae817fae8b0e6f2767668023c20480d20a
Chiara Mooney <chiaramooney@microsoft.com>
2020-09-17 10:40:33 -0700

    first commit

---
+++ windows/OrientationWindows/Class.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Class.g.h"
+
+namespace winrt::OrientationWindows::implementation
+{
+    struct Class : ClassT<Class>
+    {
+        Class() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::OrientationWindows::factory_implementation
+{
+    struct Class : ClassT<Class, implementation::Class>
+    {
+    };
+}
