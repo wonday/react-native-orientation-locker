1cf92635d5482fcf6e81fab97213b053ca63d5e5
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2020-11-16 09:09:53 -0800

    Restore example to previous state

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -22,6 +22,13 @@ if (process.argv.includes(windowsSwitch)) {
   process.argv = process.argv.filter(arg => arg !== windowsSwitch);
   process.argv.push('--config=metro.config.windows.js');
   module.exports = {
-    reactNativePath: 'node_modules/react-native-windows',
+    reactNativePath: '../node_modules/react-native-windows',
+  };
+}else {
+  module.exports = {
+    project: {
+      android: {sourceDir: './android'},
+      ios: {project: './ios/example.xcodeproj'},
+    },
   };
 }
