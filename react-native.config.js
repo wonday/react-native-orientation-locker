824ec654fb3c2d943670dd1f40e344a43821f17d
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2020-11-20 15:56:21 -0800

    Update spacing react-native.config.js
    Co-authored-by: Alexander Sklar <asklar@microsoft.com>

--- react-native.config.js
+++ react-native.config.js
@@ -8,7 +8,7 @@ module.exports = {
           {
             projectFile: 'OrientationWindows\\OrientationWindows.vcxproj',
             directDependency: true,
-		  }
+          }
         ],
       },
 	},
