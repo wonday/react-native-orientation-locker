8d131cb79018a6806762d4fc5697a08be16dcb47
namrog84 <namrog84@gmail.com>
2021-11-30 15:59:04 -0800

    be more consistent with other UNKNOWN

--- windows/OrientationWindows/OrientationWindows.cpp
+++ windows/OrientationWindows/OrientationWindows.cpp
@@ -24,7 +24,7 @@ namespace OrientationWindows {
                 cb(OrientationToString(currentOrientation));
                 return;
             }
-            cb("");
+            cb("UNKNOWN");
         });
     }
 
