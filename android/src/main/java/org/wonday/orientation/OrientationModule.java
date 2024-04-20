2e20a4cdd3020925ae2a5eb79feea22e61014378
Evert Eti <evert.etienne@gmail.com>
2024-04-20 17:06:23 +0200

    Fix reporting UI orientation upon unlock (#277)

--- android/src/main/java/org/wonday/orientation/OrientationModule.java
+++ android/src/main/java/org/wonday/orientation/OrientationModule.java
@@ -313,7 +313,7 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Ori
         isLocked = false;
 
         //force send an UI orientation event when unlock
-        lastOrientationValue = lastDeviceOrientationValue;
+        lastOrientationValue = getCurrentOrientation();
         WritableMap params = Arguments.createMap();
         params.putString("orientation", lastOrientationValue);
         if (ctx.hasActiveCatalystInstance()) {
